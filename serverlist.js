05dcf8b6af6b1853e5095668f58ecde64b14a1a9
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-09-08 19:51:36 +0300

    Update serverlist.js

--- serverlist.js
+++ serverlist.js
@@ -1,139 +1,139 @@
-const Discord = require("discord.js");
-const ownerid = ["767108799785598977"];
-const ownerid2 = ["408310632052686848"];
+const { Client, Message, MessageEmbed } = require('discord.js');
+let ownerid = "727282746614874113";
+let ownerid2 = "767108799785598977"
+
 
 module.exports = {
-  config: {
-    name: "serverlist",
-    aliases: ["slt"],
-    category: "owner",
-    description: "Displays the list of servers the bot is in!",
-    usage: " ",
-    accessableby: "Owner"
-  },
-  run: async (bot, message, args) => {
-    if (message.author.id == ownerid || ownerid2) {
-      if (!message.guild.me.hasPermission("ADMINISTRATOR"))
-        return message.channel
-          .send("I Dont Have Permissions")
-          .then(msg => msg.delete({ timeout: 5000 }));
+    name: 'serverlist',
+    aliases: ['srlist'],
+    description: 'Show the Server list Which client Joined...',
+    useage: '',
+    accessableby: "",
+    async execute(message, args, client) {
 
-      let i0 = 0;
-      let i1 = 10;
-      let page = 1;
+        if (message.author.id == ownerid || ownerid2) {
+            if (!message.guild.me.permissions.has("ADMINISTRATOR"))
+                return message.channel
+                    .send("I Dont Have Permissions")
+                    .then(msg => msg.delete({ timeout: 5000 }));
 
-      let description =
-        `Total Servers - ${bot.guilds.cache.size}\n\n` +
-        bot.guilds.cache
-          .sort((a, b) => b.memberCount - a.memberCount)
-          .map(r => r)
-          .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`)
-          .slice(0, 10)
-          .join("\n");
+            let i0 = 0;
+            let i1 = 10;
+            let page = 1;
 
-      let embed = new Discord.MessageEmbed()
-        .setAuthor(
-          message.author.tag,
-          message.author.displayAvatarURL({ dynamic: true })
-        )
-        .setColor("9e1c36")
-        .setFooter(bot.user.username)
-        .setTitle(`Page - ${page}/${Math.ceil(bot.guilds.cache.size / 10)}`)
-        .setDescription(description);
+            let description =
+                `Total Servers - ${client.guilds.cache.size}\n\n` +
+                client.guilds.cache
+                    .sort((a, b) => b.memberCount - a.memberCount)
+                    .map(r => r)
+                    .map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`)
+                    .slice(0, 10)
+                    .join("\n");
 
-      let msg = await message.channel.send(embed);
+            let embed = new MessageEmbed()
+                .setAuthor(
+                    message.author.tag,
+                    message.author.displayAvatarURL({ dynamic: true })
+                )
+                .setColor("#FF0000")
+                .setFooter(client.user.username)
+                .setTitle(`Page - ${page}/${Math.ceil(client.guilds.cache.size / 10)}`)
+                .setDescription(description);
 
-      await msg.react("⬅");
-      await msg.react("➡");
-      await msg.react("❌");
+            let msg = await message.channel.send(embed);
 
-      let collector = msg.createReactionCollector(
-        (reaction, user) => user.id === message.author.id
-      );
+            await msg.react("⬅");
+            await msg.react("➡");
+            await msg.react("❌");
 
-      collector.on("collect", async (reaction, user) => {
-        if (reaction._emoji.name === "⬅") {
-          // Updates variables
-          i0 = i0 - 10;
-          i1 = i1 - 10;
-          page = page - 1;
+            let collector = msg.createReactionCollector(
+                (reaction, user) => user.id === message.author.id
+            );
 
-          // if there is no guild to display, delete the message
-          if (i0 + 1 < 0) {
-            console.log(i0)
-            return msg.delete();
-          }
-          if (!i0 || !i1) {
-            return msg.delete();
-          }
+            collector.on("collect", async (reaction, user) => {
+                if (reaction._emoji.name === "⬅") {
+                    // Updates variables
+                    i0 = i0 - 10;
+                    i1 = i1 - 10;
+                    page = page - 1;
 
-          description =
-            `Total Servers - ${bot.guilds.cache.size}\n\n` +
-            bot.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
-              .map(r => r)
-              .map(
-                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`
-              )
-              .slice(i0, i1)
-              .join("\n");
+                    // if there is no guild to display, delete the message
+                    if (i0 + 1 < 0) {
+                        console.log(i0)
+                        return msg.delete();
+                    }
+                    if (!i0 || !i1) {
+                        return msg.delete();
+                    }
 
-          // Update the embed with new informations
-          embed
-            .setTitle(
-              `Page - ${page}/${Math.round(bot.guilds.cache.size / 10 + 1)}`
-            )
-            .setDescription(description);
+                    description =
+                        `Total Servers - ${client.guilds.cache.size}\n\n` +
+                        client.guilds.cache
+                            .sort((a, b) => b.memberCount - a.memberCount)
+                            .map(r => r)
+                            .map(
+                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`
+                            )
+                            .slice(i0, i1)
+                            .join("\n");
 
-          // Edit the message
-          msg.edit(embed);
+                    // Update the embed with new informations
+                    embed
+                        .setTitle(
+                            `Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`
+                        )
+                        .setDescription(description);
+
+                    // Edit the message
+                    msg.edit(embed);
+                }
+
+                if (reaction._emoji.name === "➡") {
+                    // Updates variables
+                    i0 = i0 + 10;
+                    i1 = i1 + 10;
+                    page = page + 1;
+
+                    // if there is no guild to display, delete the message
+                    if (i1 > client.guilds.cache.size + 10) {
+                        return msg.delete();
+                    }
+                    if (!i0 || !i1) {
+                        return msg.delete();
+                    }
+
+                    description =
+                        `Total Servers - ${client.guilds.cache.size}\n\n` +
+                        client.guilds.cache
+                            .sort((a, b) => b.memberCount - a.memberCount)
+                            .map(r => r)
+                            .map(
+                                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members\nID - ${r.id}`
+                            )
+                            .slice(i0, i1)
+                            .join("\n");
+
+                    // Update the embed with new informations
+                    embed
+                        .setTitle(
+                            `Page - ${page}/${Math.round(client.guilds.cache.size / 10 + 1)}`
+                        )
+                        .setDescription(description);
+
+                    // Edit the message
+                    msg.edit(embed);
+                }
+
+                if (reaction._emoji.name === "❌") {
+                    return msg.delete();
+                }
+
+                // Remove the reaction when the user react to the message
+                await reaction.users.remove(message.author.id);
+            });
+        } else {
+            return;
         }
 
-        if (reaction._emoji.name === "➡") {
-          // Updates variables
-          i0 = i0 + 10;
-          i1 = i1 + 10;
-          page = page + 1;
-
-          // if there is no guild to display, delete the message
-          if (i1 > bot.guilds.cache.size + 10) {
-            return msg.delete();
-          }
-          if (!i0 || !i1) {
-            return msg.delete();
-          }
-
-          description =
-            `Total Servers - ${bot.guilds.cache.size}\n\n` +
-            bot.guilds.cache
-              .sort((a, b) => b.memberCount - a.memberCount)
-              .map(r => r)
-              .map(
-                (r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} Members \nID - ${r.id}`
-              )
-              .slice(i0, i1)
-              .join("\n");
-
-          // Update the embed with new informations
-          embed
-            .setTitle(
-              `Page - ${page}/${Math.round(bot.guilds.cache.size / 10 + 1)}`
-            )
-            .setDescription(description);
-
-          // Edit the message
-          msg.edit(embed);
-        }
-
-        if (reaction._emoji.name === "❌") {
-          return msg.delete();
-        }
-
-        // Remove the reaction when the user react to the message
-        await reaction.users.remove(message.author.id);
-      });
-    } else {
-      return;
     }
-  }
-};
+}
